dd10d5ee18c97ce593ab69dcbb7e5ffd55e5cf78
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-03-16 19:04:42 +0100

    enable CORS

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
+const usersRouter = require('./src/routes/users.route');
+
 const app = express();
 const port = 8080;
 
-const usersRouter = require('./src/routes/users.route');
-
+app.use(cors())
 app.use(
     bodyParser.urlencoded({
         extended: true
     })
 );
+
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' });
