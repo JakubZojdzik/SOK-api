4416fb8e243ccfe0af4d8172e497a50d60f704f3
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-04-13 12:23:57 +0200

    Change submitted only on wrong answer

--- src/controllers/challenges.controller.js
+++ src/controllers/challenges.controller.js
@@ -122,9 +122,6 @@ const sendAnswer = (request, response) => {
                         if (!chall || !chall['answer'] || !chall['points'] || !chall['id']) {
                             return response.status(400).send('Challenge does not exist');
                         }
-
-                        pool.query("UPDATE users SET submitted=now() AT TIME ZONE 'CEST' WHERE id=$1 AND verified = true", [id]);
-
                         if (chall['answer'] === answer) {
                             pool.query('UPDATE users SET points=points+$1, solves=array_append(solves,$2) WHERE id=$3 AND verified = true', [chall['points'], chall['id'], id], (error) => {
                                 if (error) {
@@ -138,6 +135,7 @@ const sendAnswer = (request, response) => {
                                 });
                             });
                         } else {
+                            pool.query("UPDATE users SET submitted=now() AT TIME ZONE 'CEST' WHERE id=$1 AND verified = true", [id]);
                             return response.status(200).send(false);
                         }
                     }
