d9cf929e3f5b7717cc3a00601df6d091465a8dde
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-06-02 00:11:42 +0200

    Add delete announcement

--- src/controllers/announcements.controller.js
+++ src/controllers/announcements.controller.js
@@ -1,5 +1,16 @@
 const pool = require('../services/db.service');
 
+
+async function isAdmin(usrId) {
+    dbRes = await pool.query('SELECT admin FROM users WHERE id=$1 AND verified = true', [usrId]);
+    if (!dbRes || !dbRes.rows || !dbRes.rows.length) {
+        return false;
+    } else {
+        return dbRes.rows[0]['admin'] === 2;
+    }
+}
+
+
 const getAll = (request, response) => {
     pool.query('SELECT * FROM announcements ORDER BY added DESC', (error, results) => {
         if (error) {
@@ -9,6 +20,22 @@ const getAll = (request, response) => {
     });
 };
 
+const removeAnnouncement = (request, response) => {
+    const { id, annId } = request.body;
+    isAdmin(id).then((admin) => {
+        if (!admin) {
+            return response.status(403).send('You have to be admin');
+        }
+        pool.query('DELETE FROM announcements WHERE id=$1', [annId], (error) => {
+            if (error) {
+                throw error;
+            }
+            response.status(201).send('Announcement removed');
+        });
+    });
+};
+
 module.exports = {
-    getAll
+    getAll,
+    removeAnnouncement
 };
--- src/routes/announcements.route.js
+++ src/routes/announcements.route.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const announcementsController = require('../controllers/announcements.controller');
+const authenticateToken = require('../middlewares/auth');
+
+router.use('/remove', authenticateToken);
 
 router.get('/', announcementsController.getAll);
+
+router.delete('/remove', announcementsController.removeAnnouncement);
 
 module.exports = router;
