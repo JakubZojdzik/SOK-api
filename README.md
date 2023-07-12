9ef44ab6cdb06e0f9ca724270a80a9060a59e710
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-07-12 16:17:27 +0200

    Auto create tables in db

--- README.md
+++ README.md
@@ -10,56 +10,7 @@ This repository contains API of **mądrALO** writen in express and javascript.
 
 ## Environment variables
 
-`.env` file should contain these variables:
-- `CLIENT_URL` with URL of client
-- `TOKEN_SECRET` with secret for JWT
-- `SMTP_HOST` with address of smtp server
-- `SMTP_PORT` with port of smtp server
-- `SMTP_USER` with username of smtp server
-- `SMTP_PASSWORD` with password of smtp server
-- `PG_HOST` with address of postgres server
-- `PG_PORT` with port of postgres server
-- `PG_USER` with username of postgres server
-- `PG_PASSWORD` with password of postgres server
-- `PG_DB` with name of postgres databse used for api
-
-For pgadmin you can also provide:
-- `PGADMIN_LISTEN` with address that pgadmin will be listen for
-- `PGADMIN_email` with email for logging into pgadmin
-- `PGADMIN_password` with password for logging into pgadmin
-
-## Database schema
-The postgres database should contain two tables with following columns:
-
-- Table `challenges`:
-    - `id` (default: next number) - integer, primary key
-    - `title` (required) - text, title of challenge
-    - `content` (required) - text, content of challenge in html
-    - `author` (required) - text, author of challenge
-    - `points` (required) - integer, value of challenge
-    - `answer` (required) - text, correct answer to challenge
-    - `solves` (default: 0) - integer, number of users that solved this challenge
-    - `start` (default: now()) - timestampt, moment when challenge should apeare for users
-
-- Table `users`:
-    - `id` (default: next number) - integer, primary key
-    - `name` (required) - text, username
-    - `email` (required) - text, email address in schools domain
-    - `password` (required) - text, hash of password
-    - `points` (default: 0) - integer, number of points for solving challenges
-    - `solves` (default: []) - integer[], array with id's of solved challenges
-    - `admin` (defalut: 0) - integer, level of permissions
-    - `verified` (default: false) - boolean, has user verified email address
-    - `submitted` (default: 2005-04-02 21:37:00) - timestamp, moment of last answer submit
-
-`admin` has 3 levels of permissions:
-- 0: user
-- 1: teacher - can solve challenges but doesn't apear in ranking
-- 2: admin - can **EVERYTHING**
-
-## Endpoints
-
-I should create documentation but it's boring and I'm lazy, so it's nice field for your contribution xd.
+You can configure api with (.env)[/.env] file
 
 ## Run
 
@@ -72,4 +23,3 @@ docker-compose up -d
 ## Client
 
 You can find my client for **mądrALO** [here](https://github.com/JakubZojdzik/madrALO-client)
-
--- src/services/db.service.js
+++ src/services/db.service.js
@@ -10,4 +10,81 @@ const pool = new Pool({
     database: process.env.PG_DB
 });
 
+pool.query(
+    `CREATE TABLE IF NOT EXISTS public.announcements
+    (
+        id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+        title text COLLATE pg_catalog."default",
+        author text COLLATE pg_catalog."default",
+        content text COLLATE pg_catalog."default",
+        added timestamp without time zone NOT NULL DEFAULT now(),
+        CONSTRAINT announcements_pkey PRIMARY KEY (id)
+    )`,
+    (error) => {
+        if (error) {
+            throw error;
+        }
+    }
+);
+
+pool.query(
+    `CREATE TABLE IF NOT EXISTS public.challenges
+    (
+        id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+        title text COLLATE pg_catalog."default" NOT NULL,
+        content text COLLATE pg_catalog."default" NOT NULL,
+        author text COLLATE pg_catalog."default",
+        points integer NOT NULL,
+        answer text COLLATE pg_catalog."default" NOT NULL,
+        solves integer NOT NULL DEFAULT 0,
+        start timestamp without time zone NOT NULL DEFAULT now(),
+        CONSTRAINT challanges_pkey PRIMARY KEY (id)
+    )`,
+    (error) => {
+        if (error) {
+            throw error;
+        }
+    }
+);
+
+pool.query(
+    `CREATE TABLE IF NOT EXISTS public.submits
+    (
+        id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+        usr_id integer NOT NULL,
+        chall_id integer NOT NULL,
+        sent timestamp without time zone NOT NULL,
+        answer text COLLATE pg_catalog."default" NOT NULL,
+        correct boolean NOT NULL,
+        CONSTRAINT submits_pkey PRIMARY KEY (id)
+    )`,
+    (error) => {
+        if (error) {
+            throw error;
+        }
+    }
+);
+
+pool.query(
+    `CREATE TABLE IF NOT EXISTS public.users
+    (
+        id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+        name text COLLATE pg_catalog."default",
+        email text COLLATE pg_catalog."default",
+        password text COLLATE pg_catalog."default",
+        points integer NOT NULL DEFAULT 0,
+        solves integer[] NOT NULL DEFAULT ARRAY[]::integer[],
+        verified boolean NOT NULL DEFAULT false,
+        submitted timestamp without time zone NOT NULL DEFAULT '2005-04-02 21:37:00'::timestamp without time zone,
+        admin integer NOT NULL DEFAULT 0,
+        submitted_ac timestamp without time zone NOT NULL DEFAULT '2005-04-02 21:37:00'::timestamp without time zone,
+        CONSTRAINT users_pkey PRIMARY KEY (id)
+    )`,
+    (error) => {
+        if (error) {
+            throw error;
+        }
+    }
+);
+
 module.exports = pool;
