e9173561dd5e43ca23d2814e1bb5d4e1181dfa90
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-09-06 19:35:51 +0200

    Add submits for non-admins

--- src/controllers/submits.controller.js
+++ src/controllers/submits.controller.js
@@ -1,7 +1,14 @@
 const pool = require('../services/db.service');
+const isAdmin = require('../utils/isAdmin');
 
-const getAll = (request, response) => {
-    pool.query(
+const getAll = async (request, response) => {
+    const { id } = request.body;
+    const admin = await isAdmin(id);
+    if (!admin) {
+        return response.status(403).send('You dont have permissions');
+    }
+
+    return pool.query(
         `
             SELECT submits.id AS "id", users.name AS "name", challenges.title AS "title", challenges.answer AS "corr_ans",
                    submits.answer AS "given_ans", submits.correct AS "correct", submits.sent AS "sent"
@@ -21,6 +28,34 @@ const getAll = (request, response) => {
     );
 };
 
+const getById = async (request, response) => {
+    const { id } = request.body;
+    if (!id) {
+        return response.status(403).send('You are not logged in');
+    }
+
+    return pool.query(
+        `
+            SELECT submits.id AS "id", users.name AS "name", challenges.title AS "title", challenges.answer AS "corr_ans",
+                   submits.answer AS "given_ans", submits.correct AS "correct", submits.sent AS "sent"
+            FROM ((submits
+            INNER JOIN users ON submits.usr_id = users.id)
+            INNER JOIN challenges ON submits.chall_id = challenges.id)
+            WHERE users.id = $1
+            ORDER BY submits.sent DESC
+        `,
+        [id],
+        (error, dbRes) => {
+            if (error) {
+                console.error(error);
+                return response.status(500).send('Internal Server Error');
+            }
+            return response.status(200).send(dbRes.rows);
+        },
+    );
+};
+
 module.exports = {
     getAll,
+    getById,
 };
--- src/routes/submits.route.js
+++ src/routes/submits.route.js
@@ -3,19 +3,12 @@ const express = require('express');
 const router = express.Router();
 const submitsController = require('../controllers/submits.controller');
 const authenticateToken = require('../middlewares/auth');
-const isAdmin = require('../utils/isAdmin');
 const errorHandler = require('../middlewares/errorHandler');
 
 router.use('/', authenticateToken);
-router.use('/', (req, res, next) => {
-    isAdmin(req.body.id).then((admin) => {
-        if (!admin) {
-            return res.status(403).send('You have to be admin');
-        }
-        return next();
-    });
-});
+router.use('/byId', authenticateToken);
 
 router.get('/', errorHandler(submitsController.getAll));
+router.get('/byId', errorHandler(submitsController.getById));
 
 module.exports = router;
